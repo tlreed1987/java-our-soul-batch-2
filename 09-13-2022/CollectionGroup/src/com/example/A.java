0e9629329641e8c56e32771ffe74e5e727837168
srinit16 <srinit16@gmail.com>
2022-09-13 15:46:25 -0400

    Sep 13

---
+++ 09-13-2022/CollectionGroup/src/com/example/A.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class A {
+	private int number;
+	
+	private A(int number) {
+		this.number=number;
+	}
+	
+   public String toString() {
+	   return ""+number;
+   }
+
+}
---
+++ 09-13-2022/CollectionGroup/src/com/example/ATest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class ATest {
+
+	public static void main(String[] args) {
+		//create Set elements for objects A holding values 1, 4, 5, 4, 6, 8, 10, 8
+		// print and check if it skips duplicate or not. 
+		// if not ----take the help of hashcode & equals to help Set to ignore duplicate. 
+		//print again and see if only unique is printed. 
+		//               [1,4,5,6,8,10]
+	}
+
+}
