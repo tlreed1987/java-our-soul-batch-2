152a4fc8570721ac3c998db4b2e2ae5d0421d5bc
srinit16 <srinit16@gmail.com>
2022-09-15 14:18:50 -0400

    practices

---
+++ 09-15-2022/Practices/exercise_13_3/Shirt.java
@@ -0,0 +1,24 @@
+
+
+package exercise_13_3;
+
+public class Shirt extends Item {
+    private char size;
+    
+    public Shirt(double price, char size, char colorCode){
+        super ("Shirt", price, colorCode);
+        this.setSize(size);
+    }
+    
+    public void display(){
+        super.display();
+        System.out.println("\tSize: "+getSize());
+    } 
+
+    public char getSize() {
+        return size;
+    }
+    public void setSize(char size) {
+        this.size = size;
+    }
+}
