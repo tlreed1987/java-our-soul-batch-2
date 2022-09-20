05815f95a37e5a6cd602a7ca4c5065bc40d3ff62
srinit16 <srinit16@gmail.com>
2022-09-20 14:44:31 -0400

    EmployeeDao - uodated

--- MySQL-09-16/JOR2-JDBCDemo/src/com/example/dao/EmployeeDao.java
+++ MySQL-09-16/JOR2-JDBCDemo/src/com/example/dao/EmployeeDao.java
@@ -7,7 +7,7 @@ import com.example.pojos.Employee;
 public interface EmployeeDao {
 	public List<Employee> findAll() throws EmployeeException;
 	public String register(Employee e) throws EmployeeException;
-	public Employee findById(int id) throws EmployeeException;
+	//public Employee findById(int id) throws EmployeeException;
 	//public int remove(int id) throws EmployeeException();
 	//public String update(Employee e) throws EmployeeException;
 }
