068203f1da0dce530c6a640a71c4921bac3fc74e
srinit16 <srinit16@gmail.com>
2022-09-28 13:11:56 -0400

    Web-Service

--- JavaEE-Servlet-JSP/HRM-App/src/main/java/com/example/services/EmployeeJAX.java
+++ JavaEE-Servlet-JSP/HRM-App/src/main/java/com/example/services/EmployeeJAX.java
@@ -1,16 +1,27 @@
 package com.example.services;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+
+import com.example.ejb.EmployeeDao;
+import com.example.ejb.EmployeeException;
+import com.example.model.Employee;
 
 
 @WebService
 @Stateless
 public class EmployeeJAX {
-	@WebMethod
+	@EJB
+	 private EmployeeDao dao;
+	@WebMethod(operationName="greet")
 	public String get(String name) {
-		return "Good Evening"+name;
+		return "Greeting, "+name;
+	}
+	
+	public Employee find(int id) throws EmployeeException {
+		return dao.findById(id);
 	}
 
 }
--- JavaEE-Servlet-JSP/HRM-App/src/main/java/com/example/services/EmployeeRS.java
+++ JavaEE-Servlet-JSP/HRM-App/src/main/java/com/example/services/EmployeeRS.java
@@ -1,17 +1,35 @@
 package com.example.services;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
-@Stateless @Path("services")
+import com.example.ejb.EmployeeDao;
+import com.example.ejb.EmployeeException;
+import com.example.model.Employee;
+
+@Stateless @Path("emp-rest")
 public class EmployeeRS {
-	
-	@Path("name")
-    @WebMethod
-	public String get(String name) {
-		return "Good Evening"+name;
+	@EJB
+	 private EmployeeDao dao;
+	//@Path("name")
+    //@WebMethod
+	@GET
+	@Path("/{name}")
+	public String get(@PathParam("name") String name) {
+		//write a program to determine greeting message accourding to system time
+		return "Good Evening "+name;
+	}
+	@GET
+	@Path("find/{id}") @Produces("application/xml,application/json")
+	public Employee find(@PathParam("id") int id) throws EmployeeException {
+		return dao.findById(id);
 	}
 
+	
 
 }
