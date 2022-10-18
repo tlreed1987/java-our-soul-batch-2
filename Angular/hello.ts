8bd60b93c96ff683ef1dfe4c0e9f4f308f563ee0
srinit16 <srinit16@gmail.com>
2022-10-18 12:35:42 -0400

    TypeScript

--- Angular/hello.ts
+++ Angular/hello.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+export class hello{
+		private _name:string;
+		constructor(name:string){
+			this._name=name;
+
+		}
+		public get name():string{
+			return this._name;
+		}
+		public set name(name:string){
+			this._name=name;
+		}
+		
+}
+
---
+++ Angular/main.ts
@@ -0,0 +1,6 @@
+import { hello } from './hello';
+
+let h=new hello("Nitin");
+console.log(h.name);
+h.name="Aileen";
+console.log(h.name);
